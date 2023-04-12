56878be7a4ab62584589042e7864e9e992900b1c
charbs0701 <chaqhdtjr7@gmail.com>
2023-04-13 02:08:12 +0900

    리액트와 소켓 연결, 드론으로 명령전달 완료

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
-const app = require("express")();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const express = require("express");
+const http = require("http");
+const SocketIO = require("socket.io");
+require("dotenv").config();
+
+const app = express();
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+wsServer.on("connection", (socket) => {
+  console.log("Socket Connected to Browser ✅");
+
+  socket.on("takeoff", () => {
+    console.log("takeoff event received");
+    client.send(
+      takeoffCommand,
+      process.env.DRONE_PORT,
+      process.env.DRONE_HOST,
+      (err) => {
+        if (err) {
+          console.log(`Error: ${err}`);
+        } else {
+          console.log("Command sent to drone: takeoff");
+        }
+      }
+    );
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+const handleListen = () => console.log(`Listening on http://localhost:3001`);
+httpServer.listen(3001, handleListen);
+
 const sdk = require("tellojs");
 const dgram = require("dgram");
-require("dotenv").config();
 
 const client = dgram.createSocket("udp4"); // UDP socket 생성
 client.bind(process.env.DRONE_PORT); // UDP 소켓에 드론 포트 바인딩
 
-client.on("message", (message) => { // 드론으로부터 메시지 수신 시 콘솔에 출력
+client.on("message", (message) => {
+  // 드론으로부터 메시지 수신 시 콘솔에 출력
   console.log(`Message from drone: ${message}`);
 });
-
 app.post("/takeoff", (req, res) => {
   sdk.control.connect();
   client.send(
@@ -29,42 +66,15 @@ app.post("/takeoff", (req, res) => {
   );
 });
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
-  // 클라이언트로부터 이벤트를 받을 경우 실행되는 콜백 함수
-
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-});
-
-http.listen(3001, () => {
-    console.log(`http listening on *: ${process.env.PORT}`);
-});
-
 const takeoffCommand = Buffer.from("takeoff");
 
 async function connectToDrone() {
-    try {
-        await sdk.control.connect();
-        console.log("Connected to drone!");
-    } catch (err) {
-        console.log(`Connect Error: ${err}`);
-    }
+  try {
+    await sdk.control.connect();
+    console.log("Connected to drone!");
+  } catch (err) {
+    console.log(`Connect Error: ${err}`);
+  }
 }
 
 connectToDrone();
-
-
-// client.send(
-//   takeoffCommand,
-//   process.env.DRONE_PORT,
-//   process.env.DRONE_HOST,
-//   (err) => {
-//     if (err) {
-//       console.log(`Error: ${err}`);
-//     } else {
-//       console.log("Command sent to drone: takeoff");
-//     }
-//   }
-// );
