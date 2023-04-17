10b74df75d835ccc2ffe2725f3d6f3b822f64cff
charbs0701 <chaqhdtjr7@gmail.com>
2023-04-18 03:35:40 +0900

    Style

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ import { takeoffCommand, landCommand } from "./src/command.js";
 const connectToDrone = async () => {
   try {
     await sdk.control.connect();
-    console.log("Connected to drone!");
+    console.log("Connected to drone! ✅");
   } catch (err) {
-    console.log(`Drone Connect Error: ${err}`);
+    console.log(`Drone Connect Error: ${err} ❌`);
   }
 };
 
@@ -21,15 +21,16 @@ const sendCommand = (command) => {
     process.env.DRONE_HOST,
     (err) => {
       if (err) {
-        console.log(`Error: ${err}`);
+        console.log(`Error: ${err} ❌`);
       } else {
         console.log(`Command sent to drone: ${command}`);
+        console.log("-------------------------------------");
       }
     }
   );
 };
 
-const handleListen = () => console.log(`Listening on http://localhost:3001`);
+const handleListen = () => console.log(`Listening on http://localhost:3001 ✅`);
 httpServer.listen(3001, handleListen); // 웹서버 실행
 connectToDrone(); // 드론과 연결
 wsServer.on("connection", (socket) => {
@@ -37,16 +38,18 @@ wsServer.on("connection", (socket) => {
   console.log("Socket Connected to Browser ✅");
 
   socket.on("takeoff", () => {
-    console.log("takeoff event received");
+    console.log("-------------------------------------");
+    console.log("takeoff event received ✅");
     sendCommand(takeoffCommand);
   });
-  socket.on("landing", () => {
-    console.log("land event received");
+  socket.on("land", () => {
+    console.log("-------------------------------------");
+    console.log("land event received ✅");
     sendCommand(landCommand);
   });
 
   socket.on("disconnect", () => {
-    console.log("Browser disconnected");
+    console.log("Browser disconnected ❌");
   });
 });
 
